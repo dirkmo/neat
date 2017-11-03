eacac8b5ddad67454ec5eed02b2c3d2c153359fe
Dirk Möller <dirk.moeller@dreamchip.de>
2017-11-03 13:47:25 +0100

    weiter

--- source/neat.d
+++ source/neat.d
@@ -4,6 +4,19 @@ import std.algorithm;
 import std.random;
 import std.stdio;
 
+/+
+Idee für Recurrent Networks:
+
+Propagieren ist völlig Schichtunabhängig.
+Es wird zwischen jeder vollständigen Propagierung der Zustand aller Nodes gespeichert.
+ist an einem Node Ni ein rekurrentes Netz als Input angeschlossen, so wird der input des Netzes
+aus der Aktivierung des Nodes Na der vorherigen Propagierung (Zeit t-1) verwendet.
+
+Ni(t) += Na(t-1) * recurrent-net-weight
++/
+
+
+
 class ConGene {
     @disable this();
 
@@ -30,7 +43,7 @@ class ConGene {
 class ConPhenotype {
     @disable this();
 
-    this( ConGene gene ) {
+    this( const ConGene gene ) {
         weight = uniform( -1.0f, 1.0f );
         this.gene = gene;
         enabled = true;
@@ -47,7 +60,7 @@ class ConPhenotype {
     bool enabled;
 
   private:
-    ConGene gene;
+    const ConGene gene;
     float weight;
 }
 
@@ -57,21 +70,31 @@ class NodeGene {
     this( uint nodeId, Type type ) {
         this.nodeId = nodeId;
         this.type = type;
+        if( type == Type.input ) {
+            layerIndex = 0;
+        }
     }
 
     void addInputConnection( uint inno ) {
         inputCons ~= inno;
     }
 
+    const(uint[]) getInputCons() { return inputCons; }
+    const(uint[]) getOutputCons() { return outputCons; }
+
     void addOutputConnection( uint inno ) {
         outputCons ~= inno;
     }
+
+    void setLayer( int layer ) { layerIndex = layer; }
+    int  getLayer() { return layerIndex; }
 
     Type getType() { return type; }
 
   private:
     uint nodeId;
     Type type;
+    int layerIndex = -1;
     uint[] inputCons;
     uint[] outputCons;
 }
@@ -82,6 +105,7 @@ class Phenotype {
         genepool = pool;
     }
 
+    /// perform split up mutation of a random connection gene
     void mutateSplitUpConGene(float probability) {
         if( uniform(0.0f, 1.0f) < probability ) {
             uint oldCon, con1, con2;
@@ -89,8 +113,29 @@ class Phenotype {
             ConPhenotype ocp = findConPhenotype( oldCon );
             ConPhenotype cp1 = findConPhenotype( con1 );
             ConPhenotype cp2 = findConPhenotype( con2 );
-            cp1.weight = 1, cp2.weight = ocp.weight;
+            cp1.weight = 1;
+            cp2.weight = ocp.weight;
             ocp.enabled = false;
+        }
+    }
+
+    void mutateAddConPhenotype(float probability) {
+        if( uniform(0.0f, 1.0f) < probability ) {
+            uint newCon;
+            if( genepool.mutateAddNewConGene(newCon) ) {
+                // new ConGene added to gene pool
+                // create a phenotype
+                const ConGene cg = genepool.getConGene(newCon);
+                ConPhenotype cp = new ConPhenotype(cg);
+            }
+        }
+    }
+
+    /// mutate weights by probability with strength
+    void mutateWeights(float probability, float strength) {
+        // go over all connection phenotypes
+        foreach(cp; con) {
+            cp.mutateWeight(probability, strength);
         }
     }
 
@@ -111,7 +156,7 @@ class Phenotype {
 class Genepool {
     @disable this();
 
-    this( uint inputs, uint outputs, bool fullyConnected, bool enableConcurrentNets ) {
+    this( uint inputs, uint outputs, bool fullyConnected, bool enableRecurrentNets ) {
         this.inputs = inputs;
         this.outputs = outputs;
         // Input nodes have id 0..inputs-1
@@ -129,7 +174,9 @@ class Genepool {
                 }
             }
         }
-        concurrent = enableConcurrentNets;
+        recurrentAllowed = enableRecurrentNets;
+        // assign every node a layer index
+        updateNodesLayerIndex(null, 0);
     }
 
     ConGene createNewConGene( uint inputNodeId, uint outputNodeId ) {
@@ -145,13 +192,17 @@ class Genepool {
     }
 
     bool mutateAddNewConGene(ref uint newCon) {
-        // choose two nodes which are not connected
+        // choose two random nodes which are not connected
         // and connect them
         uint retry;
         uint n1, n2;
         do {
             n1 = uniform( 0, cast(uint)nodeGenes.length );
             n2 = uniform( 0, cast(uint)nodeGenes.length );
+            if( !recurrentAllowed && (nodeGenes[n1] >= nodeGenes[n2]) ) {
+                // connection would be recurrent
+                continue;
+            }
             retry++;
         } while(nodesAreConnected( n1, n2 ) && (retry < 100));
         if( retry == 100 ) {
@@ -182,6 +233,60 @@ class Genepool {
         return false;
     }
 
+    void updateNodesLayerIndex( NodeGene[] layerNodes, const int layerNumber ) {
+        if( layerNumber == 0 ) {
+            NodeGene[] list;
+            // loop over all nodes
+            foreach(n; nodeGenes) {
+                // if node is input, set layer and add connected nodes to list
+                if( n.getType == NodeGene.type.input ) {
+                    n.setLayer(0);
+                    // get every out connection of node
+                    foreach(cp; n.getOutputCons()) {
+                        // add nodes which are connected to this net
+                        const uint nodeId = conGenes[cp].endNodeId;
+                        NodeGene node = nodeGenes[nodeId];
+                        // only add hidden nodes, skip output nodes, because they get the highest layer number
+                        if( node.getType() == NodeGene.Type.hidden ) {
+                            list ~= node;
+                        }
+                    }
+                } else {
+                    // mark layer as unknown for all nodes
+                    n.setLayer(-1);
+                }
+            }
+            // recursively call for next layer
+            updateNodesLayerIndex( list, 1 );
+        } else {
+            if( layerNodes.length == 0 ) {
+                // all hidden nodes are done.
+                // now set layer of output nodes
+                foreach(o; 0..outputs) {
+                    nodeGenes[getOutputNodeId(o)].setLayer(layerNumber);
+                }
+                // recursion is done here
+                // TODO: sanity check: es dürfen keine nodes mehr mit layer=-1 übrig sein.
+            } else {
+                NodeGene[] list;
+                foreach(n; layerNodes) {
+                    n.setLayer(layerNumber);
+                    // get every out connection of node
+                    foreach(cp; n.getOutputCons()) {
+                        // add nodes which are connected to this net, if they have no layer assigned yet and aren't output neurons
+                        const uint nodeId = conGenes[cp].endNodeId;
+                        NodeGene node = nodeGenes[nodeId];
+                        if( (node.getType() == NodeGene.Type.hidden) && (node.getLayer() == -1) ) {
+                            list ~= node;
+                        }
+                    }
+                }
+                // recursively call for next layer
+                updateNodesLayerIndex( list, layerNumber + 1 );
+            }
+        }
+    }
+
     uint getInputNodeId( uint i ) {
         assert( i < inputs );
         return i;
@@ -206,7 +311,7 @@ class Genepool {
     ConGene[] conGenes;
     NodeGene[] nodeGenes;
     uint innovationCount;
-    bool concurrent;
+    bool recurrentAllowed;
     uint inputs;
     uint outputs;
 }
